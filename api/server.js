e89eb59046e23103e4cdfacf7ec73c01e46bba42
cainpalmer <cain.a.palmer@gmail.com>
2021-12-09 16:21:10 -0500

    cleaned up server.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
-const express = require('express')
 
+// Server Variables
+const express = require('express')
+const recipesRouter = require('./recipes/recipes-router')
 const server = express()
 
+
+// Use Server
 server.use(express.json())
 
 server.use('/api/recipes', recipesRouter)
+
+// Exports
+module.exports = server
