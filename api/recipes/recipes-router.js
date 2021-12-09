182269a3ef3204a9fe95a8292225fe97ad05418b
cainpalmer <cain.a.palmer@gmail.com>
2021-12-09 17:58:29 -0500

    done

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const Recipe = require('./recipes-model')
+const express = require('express')
 
 router.get('/:recipe_id', (req, res, next) => {
     Recipe.getRecipeById(req.params.recipe_id)
